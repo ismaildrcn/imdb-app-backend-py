8e0e7a79127da8ba0265f5a0b48770e229de8a73
İsmail Durcan <ismaildurcan40@gmail.com>
2025-11-01 18:37:08 +0300

    Remove unused root and user endpoint definitions

--- main.py
+++ main.py
@@ -10,11 +10,3 @@ app.include_router(movie.router, prefix="/v1/api")
 app.include_router(user.router, prefix="/v1/api")
 
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from API!"}
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id, "name": "İsmail"}
